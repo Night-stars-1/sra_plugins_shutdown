38d6f6dff062e126b8e8d3daa6ae797ef88eda4b
Night-stars-1 <nujj1042633805@gmail.com>
2023-06-08 14:41:42 +0800

    fix: 更新适配

--- sra_plugins_shutdown/main.py
+++ sra_plugins_shutdown/main.py
@@ -2,7 +2,7 @@
 Author: Night-stars-1 nujj1042633805@gmail.com
 Date: 2023-06-08 14:19:52
 LastEditors: Night-stars-1 nujj1042633805@gmail.com
-LastEditTime: 2023-06-08 14:30:50
+LastEditTime: 2023-06-08 14:41:36
 Description: 
 
 Copyright (c) 2023 by Night-stars-1, All Rights Reserved. 
@@ -13,7 +13,7 @@ from utils.log import log
 from subprocess import run, DEVNULL
 
 @hookimpl
-def stop(SRA):
+def end(SRA):
     shell = ["shutdown", "-s", "-f", "-t", "60"]
     result = run(shell, shell=True, capture_output=True)
     log.info("将在{shutdown_time}后关机".format(shutdown_time=result.stdout))
